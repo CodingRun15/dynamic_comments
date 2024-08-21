5800c5d8a36b2fb6b0e45cd1c718364f91e171bd
CodingRun15 <154725027+CodingRun15@users.noreply.github.com>
2024-08-22 00:44:22 +0530

    ja

--- src/services/firebase.tsx
+++ src/services/firebase.tsx
@@ -1,17 +1,16 @@
 import { initializeApp } from "firebase/app";
-import React from 'react';
-
+const  key = process.env.REACT_APP_API_KEY;
+console.log(key);
 const firebaseConfig = {
-    apiKey: "AIzaSyAVb7L09_AKbTqtseLZQ-OZUSSzPHu3Dgg",
-    authDomain: "dynamiccomments.firebaseapp.com",
-    projectId: "dynamiccomments",
-    storageBucket: "dynamiccomments.appspot.com",
-    messagingSenderId: "60018102676",
-    appId: "1:60018102676:web:05437d89c1b71027733f81",
-    measurementId: "G-SN1SSYZ54E",
-    databaseURL: "https://dynamiccomments-default-rtdb.firebaseio.com/"
-    
-  };
+  api_Key: key,
+  authDomain: "dynamiccomments.firebaseapp.com",
+  projectId: "dynamiccomments",
+  storageBucket: "dynamiccomments.appspot.com",
+  messagingSenderId: "60018102676",
+  appId: "1:60018102676:web:05437d89c1b71027733f81",
+  measurementId: "G-SN1SSYZ54E",
+  databaseURL: 'https://dynamiccomments-default-rtdb.firebaseio.com'
   
+};
   // Initialize Firebase
  export const app = initializeApp(firebaseConfig);
